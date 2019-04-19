56ad0879ca0ba1e72b730bb8170b5fdc9117d2f5
mahsum <mahsum@webjump.com.br>
2019-04-19 12:14:26 -0300

    24 adding new changes

--- src/components/Tab/index.jsx
+++ src/components/Tab/index.jsx
@@ -1,48 +1,83 @@
-import React from 'react';
-import ControlledTabs from './TabContent/index';
-import { TabContainer } from './styles';
+import React, { Component, Fragment } from 'react';
+import { TabContainer, Content, Tabs, Title, TitleDiv } from './styles';
+import colors from '../../helpers/styles/colors';
 
 const tabs = [
     {
         title: 'Sobre o Produto',
-        content:
-            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' +
-            "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled " +
-            'it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the ' +
-            '1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
     },
     {
         title: 'Informações técnicas',
-        content:
-            'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. ' +
-            "The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', " +
-            "making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' " +
-            'will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
     },
     {
         title: 'Avaliações',
-        content:
-            'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, ' +
-            "or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't " +
-            'anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, ' +
-            'making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, ' +
-            'to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.',
     },
 ];
 
-const TabComponent = () => {
-    return (
-        <TabContainer>
-            {tabs &&
-                tabs.map((item, index) => {
-                    return (
-                        <ControlledTabs key={index} title={item.title}>
-                            {item.content}
-                        </ControlledTabs>
-                    );
-                })}
-        </TabContainer>
-    );
-};
+class TabComponent extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            active: '',
+            displayContent: false,
+            content:
+                'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, ' +
+                "or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't " +
+                'anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, ' +
+                'making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, ' +
+                'to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.',
+        };
+
+        this.showTabContent = this.showTabContent.bind(this);
+        this.hideTabContent = this.hideTabContent.bind(this);
+    }
+
+    showTabContent() {
+        this.setState({ displayContent: true }, () => {
+            document.addEventListener('click', this.hideTabContent);
+        });
+    }
+
+    hideTabContent() {
+        this.setState({ displayContent: false }, () => {
+            document.removeEventListener('click', this.hideTabContent);
+        });
+    }
+    render() {
+        return (
+            <Fragment>
+                <TabContainer>
+                    {tabs &&
+                        tabs.map((item, index) => {
+                            return (
+                                <Tabs key={index}>
+                                    <TitleDiv
+                                        style={
+                                            this.state.displayContent
+                                                ? { backgroundColor: colors.primary }
+                                                : { backgroundColor: colors.alternativeLight }
+                                        }
+                                        onClick={this.showTabContent}
+                                    >
+                                        <Title
+                                            style={
+                                                this.state.displayContent
+                                                    ? { color: colors.white }
+                                                    : { color: colors.alternative }
+                                            }
+                                        >
+                                            {item.title}
+                                        </Title>
+                                    </TitleDiv>
+                                </Tabs>
+                            );
+                        })}
+                </TabContainer>
+                <Content>{this.state.displayContent ? this.state.content : null}</Content>
+            </Fragment>
+        );
+    }
+}
 
 export default TabComponent;
--- src/components/Tab/styles.js
+++ src/components/Tab/styles.js
@@ -4,9 +4,36 @@ const TabContainer = styled.div`
     display: inline-flex;
     width: 100%;
     height: auto;
-    border: 1px solid ${colors.alternativeLight};
     background-color: ${colors.white};
     border-radius: 5px;
 `;
 
-export { TabContainer };
+const Tabs = styled.div`
+    width: 100%;
+    text-align: center;
+`;
+
+const TitleDiv = styled.div`
+    height: 60px;
+    border: 1px solid ${colors.alternativeLight};
+    background-color: ${colors.alternativeLight};
+`;
+
+const Title = styled.span`
+    display: block;
+    margin: 15px auto;
+    font-family: ${fonts.fontbase};
+    font-size: ${sizes.large};
+    font-weight: 700;
+    line-height: 30px;
+`;
+const Content = styled.div`
+    padding: 5%;
+    text-align: left;
+    line-height: 20px;
+    line-space: 4px;
+    line-break: auto;
+    border: 1px solid ${colors.alternativeLight};
+`;
+
+export { TabContainer, Tabs, TitleDiv, Title, Content };
